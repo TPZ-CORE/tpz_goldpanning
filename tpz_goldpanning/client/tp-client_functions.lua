9d67ed7c4449838cc11a083e335bbd7d91b20a2d
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-02 19:20:23 +0200

    Update tp-client_functions.lua

--- tpz_goldpanning/client/tp-client_functions.lua
+++ tpz_goldpanning/client/tp-client_functions.lua
@@ -1,44 +1,64 @@
+
+local CURRENT_OBJECT_ENTITY = nil
+
 -----------------------------------------------------------
 --[[ Functions  ]]--
 -----------------------------------------------------------
 
 function CrouchAnimAndAttach()
+
     local dict = "script_rc@cldn@ig@rsc2_ig1_questionshopkeeper"
     RequestAnimDict(dict)
+
     while not HasAnimDictLoaded(dict) do
         Citizen.Wait(10)
     end
 
-    local ped = PlayerPedId()
-    local coords = GetEntityCoords(ped)
+    local ped       = PlayerPedId()
+    local coords    = GetEntityCoords(ped)
     local boneIndex = GetEntityBoneIndexByName(ped, "SKEL_R_HAND")
-    local modelHash = joaat("P_CS_MININGPAN01X")
-    LoadModel(modelHash)
-    goldPanObject = CreateObject(modelHash, coords.x+0.3, coords.y,coords.z, true, false, false)
-    SetEntityVisible(goldPanObject, true)
-    SetEntityAlpha(goldPanObject, 255, false)
-    Citizen.InvokeNative(0x283978A15512B2FE, goldPanObject, true)
-    SetModelAsNoLongerNeeded(modelHash)
-    AttachEntityToEntity(goldPanObject,ped, boneIndex, 0.2, 0.0, -0.2, -100.0, -50.0, 0.0, false, false, false, true, 2, true)
 
+    LoadModel("P_CS_MININGPAN01X")
+
+    CURRENT_OBJECT_ENTITY = CreateObject(joaat("P_CS_MININGPAN01X"), coords.x+0.3, coords.y,coords.z, true, false, false)
+    
+    SetEntityVisible(CURRENT_OBJECT_ENTITY, true)
+    SetEntityAlpha(CURRENT_OBJECT_ENTITY, 255, false)
+    Citizen.InvokeNative(0x283978A15512B2FE, CURRENT_OBJECT_ENTITY, true)
+    
+    SetModelAsNoLongerNeeded(joaat("P_CS_MININGPAN01X"))
+
+    AttachEntityToEntity(CURRENT_OBJECT_ENTIT,ped,boneIndex, 0.2, 0.0, -0.2, -100.0, -50.0, 0.0, false, false, false, true, 2, true)
     TaskPlayAnim(ped, dict, "inspectfloor_player", 1.0, 8.0, -1, 1, 0, false, false, false)
 end
 
 function GoldShake()
     local dict = "script_re@gold_panner@gold_success"
     RequestAnimDict(dict)
+    
     while not HasAnimDictLoaded(dict) do
         Citizen.Wait(10)
     end
+
     TaskPlayAnim(PlayerPedId(), dict, "SEARCH02", 1.0, 8.0, -1, 1, 0, false, false, false)
 end
 
-function LoadModel(model)
-    local attempts = 0
-    while attempts < 100 and not HasModelLoaded(model) do
-        RequestModel(model)
+LoadModel = function(inputModel)
+    local model = joaat(inputModel)
+ 
+    RequestModel(model)
+ 
+    while not HasModelLoaded(model) do RequestModel(model)
         Citizen.Wait(10)
-        attempts = attempts + 1
     end
-    return IsModelValid(model)
+ end
+
+RemoveEntityProperly = function(entity, objectHash)
+    DeleteEntity(entity)
+    DeletePed(entity)
+    SetEntityAsNoLongerNeeded( entity )
+
+    if objectHash then
+        SetModelAsNoLongerNeeded(objectHash)
+    end
 end
