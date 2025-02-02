0a029fa1f6df88b1b382fcb1feb92fb7f99c1645
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-03 00:57:19 +0200

    Update locales.lua

--- tpz_goldpanning/locales.lua
+++ tpz_goldpanning/locales.lua
@@ -2,11 +2,15 @@
 Locales = {}
 
 Locales = {
-  ['SUCCESSFULLY_FOUND']          = "~o~You succesffully found X%s Gold Fragments",
+  ['SUCCESSFULLY_FOUND']                    = "~o~You succesffully found X%s Gold Fragments",
+  
+  ['NOT_FOUND']                             = "~e~Unfortunately, You haven't found anything.",
+  ['NOT_ALLOWED_AREA']                      = "~e~You cannot pan for gold fragments in this area.",
 
-  ['NOT_FOUND']                    = "~e~Unfortunately, You haven't found anything.",
-  ['NOT_ALLOWED_AREA']             = "~e~You cannot pan for gold fragments in this area.",
+  ['NOT_ENOUGH_INVENTORY_WEIGHT']           = "~e~You do not have enough weight to carry any Gold Fragments.",
+  ['NO_DURABILITY']                         = "~e~The Gold Pan you have been using, unfortunately seems to be useless.",
+  ['ALREADY_IN_PROGRESS']                   = "~e~Cannot pan while already doing this action.",
 
-  ['NOT_ENOUGH_INVENTORY_WEIGHT'] = "~e~You do not have enough weight to carry any Gold Fragments.",
-  ['NO_DURABILITY']               = "~e~The Gold Pan you have been using, unfortunately seems to be useless.",
+  ['DEVTOOLS_INJECTION_DETECTED_TITLE_LOG'] = "DevTools / Injection Cheat Found!",
+  ['DEVTOOLS_INJECTION_DETECTED']           = "You have been kicked due to cheating by using DevTools or Injection cheat.",
 }
