9a2da6dba7dd644de60e9b89f9f465ace1282024
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-03 00:33:38 +0200

    Update config.lua

--- tpz_goldpanning/config.lua
+++ tpz_goldpanning/config.lua
@@ -17,6 +17,20 @@ Config.Difficulties = {
     { mode = "hard" },
 }
 
+---------------------------------------------------------------
+--[[ Webhooks ]]--
+---------------------------------------------------------------
+
+Config.Webhooks = {
+    
+    ['DEVTOOLS_INJECTION_CHEAT'] = { -- Warnings and Logs about players who used or atleast tried to use devtools injection.
+        Enabled = false, 
+        Url = "", 
+        Color = 10038562,
+    },
+
+}
+
 -----------------------------------------------------------
 --[[ Notification Functions  ]]--
 -----------------------------------------------------------
