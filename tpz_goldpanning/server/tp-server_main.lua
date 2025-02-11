aa00022ebce3e4d02c5c750339d49f5ca22118b9
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-11 23:14:29 +0200

    Update tp-server_main.lua

--- tpz_goldpanning/server/tp-server_main.lua
+++ tpz_goldpanning/server/tp-server_main.lua
@@ -1,61 +1,11 @@
-local TPZ = exports.tpz_core:getCoreAPI()
+local TPZ    = exports.tpz_core:getCoreAPI()
+local TPZInv = exports.tpz_inventory:getInventoryAPI() -- Getting the inventory API Functions.
+
 local ListedPlayers = {}
 
 -----------------------------------------------------------
---[[ Local Functions ]]--
------------------------------------------------------------
-
--- @GetTableLength returns the length of a table.
-local function GetTableLength(T)
-    local count = 0
-    for _ in pairs(T) do count = count + 1 end
-    return count
-end
-
-local function NearestValue(table, number)
-    local smallestSoFar, smallestIndex
-    for i, y in ipairs(table) do
-        if not smallestSoFar or (math.abs(number-y.chance) < smallestSoFar) then
-            smallestSoFar = math.abs(number-y.chance)
-            smallestIndex = i
-        end
-    end
-    return smallestIndex, table[smallestIndex]
-end
-
-local function GetRandomReward(inputLocation)
-
-	local rewardList  = {}
-	local rewardAdded = false
-
-	if GetTableLength(Config.Items[inputLocation]) > 0 then
-		
-		for k,v in pairs(Config.Items[inputLocation]) do 
-		
-			math.randomseed(os.time()) -- required to refresh the random.math for better results. 
-			local chance = math.random(1, 100)
-	
-			if v.chance >= chance then
-				table.insert(rewardList, v)
-				rewardAdded = true
-			end
-	
-		end
-
-	end
-
-	if rewardAdded then
-		math.randomseed(os.time()) -- required to refresh the random.math for better results. 
-
-		local chance = math.random(1, 100)
-		local index, value = NearestValue(rewardList, chance)
-		return (value)
-
-	else
-		return ({Item = "nothing"})
-	end
-
-end
+--[[ Local Functions  ]]--
+-------------------------------------------------------------
 
 local function GetPlayerData(source)
 	local _source = source
@@ -66,29 +16,46 @@ local function GetPlayerData(source)
         username       = xPlayer.getFirstName() .. ' ' .. xPlayer.getLastName(),
 		identifier     = xPlayer.getIdentifier(),
         charIdentifier = xPlayer.getCharacterIdentifier(),
-		job            = xPlayer.getJob(),
 	}
 
 end
 
-local function HasRequiredJob(currentJob)
+local IsWaterSource = function(currentWaterId)
 
-	if not Config.Jobs or Config.Jobs and GetTableLength(Config.Jobs) <= 0 then
-		return true
-	end
+    for k,v in pairs(Config.WaterTypes) do
+        if currentWaterId == Config.WaterTypes[k]["waterhash"] then
+            return true
+        end
+    end
 
-	for _, job in pairs (Config.Jobs) do
-
-		if job == currentJob then
-			return true
-		end
-		
-	end
-
-	return false
+    return false
 
 end
 
+-----------------------------------------------------------
+--[[ Items Registration  ]]--
+-----------------------------------------------------------
+
+TPZInv.registerUsableItem(Config.GoldPanItem, "tpz_goldpanning", function(data)
+	local _source = data.source
+
+    ListedPlayers[_source] = nil
+
+	if data.durability <= 0 and Config.Durability.Enabled then
+	    SendNotification(_source, Locales['NO_DURABILITY'], "error")
+	    return
+	end
+
+	TriggerClientEvent('tpz_goldpanning:client:startPanning', _source)
+	
+    if Config.Durability.Enabled then
+        math.randomseed(os.time()) -- required to refresh the random.math for better results. 
+        local randomValueRemove = math.random(Config.Durability.RemoveValue.min, Config.Durability.RemoveValue.max)
+			
+        TPZInv.removeItemDurability(_source, Config.GoldPanItem, randomValueRemove, data.itemId, false)
+    end
+
+end)
 -----------------------------------------------------------
 --[[ Base Events  ]]--
 -----------------------------------------------------------
@@ -101,73 +68,58 @@ AddEventHandler('onResourceStop', function(resourceName)
     ListedPlayers = nil -- clearing all data
 end)
 
------------------------------------------------------------
---[[ Events ]]--
------------------------------------------------------------
+AddEventHandler('playerDropped', function (reason, resourceName, reason)
+    ListedPlayers[source] = nil -- removing player source in case he is listed (crashed?)
+end)
 
-RegisterServerEvent("tpz_mining:server:success")
-AddEventHandler("tpz_mining:server:success", function(targetMiningLocation, targetItemId)
-	local _source    = source
-	local PlayerData = GetPlayerData(_source)
-    local xPlayer    = TPZ.GetPlayer(_source)
+-------------------------------------------------------------
+--[[ Events  ]]--
+-------------------------------------------------------------
 
-	math.randomseed(os.time())
-	
-	Wait( math.random(100, 250) )
+RegisterServerEvent("tpz_goldpanning:server:onRandomReward")
+AddEventHandler("tpz_goldpanning:server:onRandomReward", function(waterHashId)
+    local _source          = source 
+    local PlayerData       = GetPlayerData(_source)
+    local xPlayer          = TPZ.GetPlayer(_source)
 
-	if (targetMiningLocation == nil) or (targetMiningLocation and Config.Items[targetMiningLocation] == nil) or (not hasRequiredJob) or (ListedPlayers[_source]) then
+    local foundWaterSource = IsWaterSource(waterHashId)
+
+    if ListedPlayers[_source] or not foundWaterSource then
 
         if Config.Webhooks['DEVTOOLS_INJECTION_CHEAT'].Enabled then
             local _w, _c      = Config.Webhooks['DEVTOOLS_INJECTION_CHEAT'].Url, Config.Webhooks['DEVTOOLS_INJECTION_CHEAT'].Color
-            local description = 'The specified user attempted to use devtools / injection or netbug cheat on mining reward.'
+            local description = 'The specified user attempted to use devtools / injection or netbug cheat on gold panning reward.'
             TPZ.SendToDiscordWithPlayerParameters(_w, Locales['DEVTOOLS_INJECTION_DETECTED_TITLE_LOG'], _source, PlayerData.steamName, PlayerData.username, PlayerData.identifier, PlayerData.charIdentifier, description, _c)
         end
 
-		ListedPlayers[_source] = nil
+        ListedPlayers[_source] = nil
         xPlayer.disconnect(Locales['DEVTOOLS_INJECTION_DETECTED'])
+        return
+    end
 
-		return
-	end
+    ListedPlayers[_source] = true
 
-	ListedPlayers[_source] = true
+    math.randomseed(os.time()) -- required to refresh the random.math for better results. 
 
-	-- Removing durability if enabled on action.
-	if Config.Durability.Enabled and targetItemId then
-		local randomValueRemove = math.random(Config.Durability.RemoveValue.min, Config.Durability.RemoveValue.max)
-		xPlayer.removeItemDurability(Config.PickaxeItem, randomValueRemove, targetItemId, false)
-	end
+    local randomRewardCount = math.random(Config.Reward.ReceiveValue.min, Config.Reward.ReceiveValue.max)
+    local canCarryItem      = xPlayer.canCarryItem(Config.Reward.Item, randomRewardCount)
 
-	local RewardItem = GetRandomReward(targetMiningLocation)
+    Wait(500)
 
-	if RewardItem.Item ~= "nothing" then
+    if canCarryItem then
 
-		math.randomseed(os.time()) -- required to refresh the random.math for better results. 
-		local randomQuantity = math.random(RewardItem.quantity.min, RewardItem.quantity.max)
+        xPlayer.addItem(Config.Reward.Item, randomRewardCount)
 
-		if Config.tpz_leveling then
+        SendNotification(_source, string.format(Locales['SUCCESSFULLY_FOUND'], randomRewardCount), "success")
+        
+    else
+        SendNotification(_source, Locales['NOT_ENOUGH_INVENTORY_WEIGHT'], "error")
+    end
 
-			local LevelingAPI = exports.tpz_leveling:getAPI()
-			LevelingAPI.AddPlayerLevelExperience(_source, 'mining', RewardItem.experience )
-		end
-
-		local canCarryItem = xPlayer.canCarryItem(RewardItem.item, randomQuantity)
-
-		Wait(500)
-		if canCarryItem then
-
-			xPlayer.addItem(RewardItem.item, randomQuantity, nil)
-
-			SendNotification(_source, string.format(Locales['SUCCESSFULLY_FOUND_REWARD'], randomQuantity, RewardItem.label), "success")
-			
-		else
-			SendNotification(_source, Locales['CANNOT_CARRY'], "error")
-		end
-
-	else
-		SendNotification(_source, Locales['FOUND_NOTHING'], "error")
-	end
-
-	Wait(3000)
-	ListedPlayers[_source] = nil
+    -- there is no chance winning the skillcheck within 5 seconds, 
+    -- there are also animations at start, within those seconds, the player should
+    -- do animations, its 100% devtools. 
+    Wait(5000)  
+    ListedPlayers[_source] = nil
 
 end)
